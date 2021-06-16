448737e598519cbbf11b712a3cda1efe70c03273
MasayaAoyama <amsy810@gmail.com>
2021-06-17 04:11:22 +0900

    fix

--- install-argocd.sh
+++ install-argocd.sh
@@ -1,7 +1,11 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-echo "password is following"
+
+echo "password is following (user: admin)"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo ""
 echo "please visit http://localhost:80"
+
+kubectl apply -f app.yaml
+
